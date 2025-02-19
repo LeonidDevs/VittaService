928e0e4b9c7011acb2f24a9bc69eaa6e62f4147c
straponessi <l.a.straponessi@gmail.com>
2025-02-19 21:27:50 +0300

    fxes

---
+++ DTO/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using Service.Models;
+
+namespace Service.DTO
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
